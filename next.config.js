43c33383a624f0dee7001105b5c65ad03bb6afa1
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-08-09 09:54:08 -0600

    build(nextjs): set BUILD_ID when compiling NextJS files

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   poweredByHeader: false,           // https://nextjs.org/docs/api-reference/next.config.js/disabling-x-powered-by
+  generateBuildId: async () => {    // https://nextjs.org/docs/api-reference/next.config.js/configuring-the-build-id
+    return process.env.NEXT_PUBLIC_BUILD_ID || "development"
+  },
 }
 
 module.exports = nextConfig
