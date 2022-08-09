d1e079ec9c21df9f6f8f3973de095924c5141b6f
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-08-09 09:53:10 -0600

    build(nextjs): remove http header 'x-powered'

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  poweredByHeader: false,           // https://nextjs.org/docs/api-reference/next.config.js/disabling-x-powered-by
 }
 
 module.exports = nextConfig
